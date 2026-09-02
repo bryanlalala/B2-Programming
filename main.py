5111d2b2f88bf97d7306360e0a668002cd99c77c
bryan123260 <bryan123260@mcpsva.org>
2026-09-02 11:56:35 -0400

    hi

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-
-
-
-print("Hello World")
-
-
+"""
+Filename: interview.py
+Author: <Nunez, Bryan>
+Created: <09/02/2026>
+Instructor: Burgess
+"""
+x=input("Why would you be good for this job?")
+print(x)
+x=input("How do you handle stress and pressure?")
+print(x)
+x=input("What type of work environment do you thrive in")
+print(x)
+x=input("How do you give constructive feedback to a colleague?")
+print(x)
