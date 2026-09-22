7ca1c5e357c128aeec5921d6f644830a85b05e1e
bryan123260 <bryan123260@mcpsva.org>
2026-09-22 10:40:36 -0400

    .

---
+++ simpleCalculator.py
@@ -0,0 +1,23 @@
+"""
+Filename: simple_calculator.py
+Author: <Nunez, Bryan>
+Created: <09/22/2026>
+Instructor: Burgess
+"""
+
+#step 1
+print("Welcome to simple calculator.")
+print("This calculator will ask the user to input two numbers,")
+print("then the program will perform the four basic operations on the two numbers")
+print(" (add, subtract, multiply, and divide).")
+
+#step 2
+print("insert first number on the line below")
+
+
+#step 3
+print("Please enter your operation.")
+print("For addition please input: +")
+print("For subtraction please input: -")
+print("For multiplication please input: *")
+print("For division please input: /")
