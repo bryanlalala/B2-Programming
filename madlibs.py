0540b17370f8e9260d8de262a1f9e00fb0805b74
bryan123260 <bryan123260@mcpsva.org>
2026-09-22 11:04:32 -0400

    .

---
+++ madlibs.py
@@ -0,0 +1,8 @@
+Q1 = input("enter the adjective ")
+Q2 = input("enter the noun")
+Q3 = input("enter a proper noun (name)")
+Q4 = input("enter the noun(place)")
+Q5 = input("enter the plural noun")
+print(f"It was a {Q1} morning when Captain {Q2} and their trusted co-pilot, {Q3}, \n"
+      f"boarded the USS {Q4}. Their mission was simple: deliver a cargo load of hyper-advanced "
+      f"{Q5} to the furthest galaxy.")
