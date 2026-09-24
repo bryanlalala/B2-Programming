e1d4432c98359fef311d0ad3904d4870b55391ac
bryan123260 <bryan123260@mcpsva.org>
2026-09-24 11:05:31 -0400

    .

--- madlibs.py
+++ madlibs.py
@@ -1,8 +1,26 @@
 Q1 = input("enter the adjective ")
-Q2 = input("enter the noun")
-Q3 = input("enter a proper noun (name)")
-Q4 = input("enter the noun(place)")
-Q5 = input("enter the plural noun")
+Q2 = input("enter the noun ")
+Q3 = input("enter a proper noun (name) ")
+Q4 = input("enter the noun(place) ")
+Q5 = input("enter the plural noun ")
+Q6 = input("enter the adjective ")
+Q7 = input("enter the body part ")
+Q8 = input("enter the verb (ending in -ing ")
+Q9 = input("enter the noun")
+Q10 = input("enter the animal ")
+Q11 = input("enter the adjective ")
+Q12 = input("enter the exclamation ")
+Q13 = input("enter the liquid ")
+Q14 = input("enter the enter the verb ")
+Q15 = input("enter the plural noun ")
+Q16 = input("enter the number ")
+Q17 = input("enter the adjective ")
+Q18 = input("enter the noun ")
+Q19 = input("enter the verb ")
+Q20 = input("enter the adjective ")
 print(f"It was a {Q1} morning when Captain {Q2} and their trusted co-pilot, {Q3}, \n"
       f"boarded the USS {Q4}. Their mission was simple: deliver a cargo load of hyper-advanced "
-      f"{Q5} to the furthest galaxy.")
+      f"{Q5} to the furthest galaxy. Suddenly, a {Q6} asteroid collided with the ship's control panel! \n"
+      f" The impact caused the main engine to shake like a giant {Q7}. Sirens began {Q8} loudly throughout the deck. grab the emergency {Q9}! \n"
+      f" yelled the Captain, but it was too late. The shit was caught in the gravitational pull of a giant glowing space{Q10}"
+      f"")
